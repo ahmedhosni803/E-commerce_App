f9ef1c702dcf5602e7bf7c2f971d42de4b4eb1eb
Ahmed <Ahmedhosni803@gmail.com>
2023-07-28 02:06:09 +0300

    Finished App

---
+++ lib/features/sign_up/domain/entities/user_data.dart
@@ -0,0 +1,11 @@
+class UserData {
+  String name;
+  String phone;
+  String password;
+  String email;
+
+  UserData({required this.name,
+    required this.phone,
+    required this.password,
+    required this.email});
+}
